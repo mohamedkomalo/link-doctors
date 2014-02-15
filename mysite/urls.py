08ae326983b1e31ed6487f69406d5e78524c6309
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-02-15 15:47:05 +0400

    working with templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from mysite.views import viewsModule
+
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^$', viewsModule.index, name='index'),
     url(r'^linkedin_login/', 'mysite.views.linkedin', 'login'),
-
+	
+    url(r'^doctor/(?P<doctor_id>\d+)$', viewsModule.show_doctor, name='show_doctor'),
+	
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ mysite/views/viewsModule.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.http import Http404
+from mysite.models import *
+from django.template import RequestContext, loader
+
+def index(request):
+	return HttpResponse("Index Page")
+
+def show_doctor(request, doctor_id):
+  doctor=Doctor.objects.filter(id=doctor_id).first()
+
+  if not doctor:
+	  raise Http404
+
+  template = loader.get_template('doctor.html')
+  context = RequestContext(request, {
+    'doctor': doctor,
+  })
+  
+  return HttpResponse(template.render(context))
