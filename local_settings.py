a11269cf0ebbbe3e51fbdf7346b28a1d760d70f1
Samer Atiani <satiani@gmail.com>
2014-02-16 09:25:29 +0400

    Adding local_settings.py so that we can get up and running fast

---
+++ local_settings.py
@@ -0,0 +1,5 @@
+# defining authentication parameters:
+API_KEY = "75ylhmbcn06a14"
+API_SECRET = "tjEBRScvRSTRaKKH"
+USER_TOKEN = "27d39247-596b-49fd-8702-dfb6984ea420"
+USER_SECRET = "3b7ff8d5-21e0-4532-b261-041d23ed8363"
